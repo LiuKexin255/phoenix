0739942906054a0ea141aded118b3c8c6bd983c6
Liu Kexin <l915374208@outlook.com>
2024-08-03 16:07:40 +0000

    先这样把

--- project/infra/object_storage/storage/client/client.go
+++ project/infra/object_storage/storage/client/client.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	"phoenix/project/infra/object_storage/storage/proto"
+	proto "phoenix/project/infra/object_storage"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
